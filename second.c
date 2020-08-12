68eb2c275587868297ae06e5124f0ce47bb5c201
shyampavan <shyampavan447@gmail.com>
2020-08-12 15:48:35 +0530

    using struct

---
+++ second.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <math.h>
+
+
+
+
+struct student{
+int userid;
+char firstname[25];
+char lastname[25];
+int  age;
+};
+
+
+int main()
+{
+
+
+
+
+struct student shyam;
+struct student shiva;
+
+puts("enter shyam info :");
+
+printf("\nenter userid :\n");
+scanf("%d",&shyam.userid);
+
+printf("\nenter firstname :\n");
+scanf("%s",shyam.firstname);
+
+printf("\nenter lastname :\n");
+scanf("%s",shyam.lastname);
+
+printf("\nenter age :\n");
+scanf("%d",&shyam.age);
+
+printf("\nuserid : %d\n", shyam.userid);
+
+printf("\nname :%s %s\n" , shyam.firstname,shyam.lastname);
+
+printf("\nage :%d\n ",shyam.age);
+
+puts("___________________________________________");
+puts("enter shiva info :");
+
+printf("\nenter userid :\n");
+scanf("%d",&shiva.userid);
+
+printf("\nenter firstname :\n");
+scanf("%s",shiva.firstname);
+
+printf("\nenter lastname :\n");
+scanf("%s",shiva.lastname);
+
+printf("\nenter age :\n");
+scanf("%d",&shiva.age);
+
+printf("\nuserid : %d\n", shiva.userid);
+
+printf("\nname :%s %s\n" , shiva.firstname,shiva.lastname);
+
+printf("\nage :%d\n ",shiva.age);
+
+}
