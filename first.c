a20ce1cb69e6c2569732c42e9ffd030e92bd0875
shyampavan <shyampavan447@gmail.com>
2020-08-12 15:22:53 +0530

    modified

--- first.c
+++ first.c
@@ -2,6 +2,7 @@
 #include<math.h>
 #include<ctype.h>
 int main(){
+  printf("\nhey i am writing this line just for my reference to git \n");
   printf("\nHello shyam! welcome to programming\n");
   return 0;
 }
