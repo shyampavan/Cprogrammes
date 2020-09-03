ade2322e154dc66cade4a311352442e1fed0d15c
shyampavan <shyampavan447@gmail.com>
2020-09-03 09:50:19 +0530

    designpatterns

---
+++ patternsAsc.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+char main(void){
+     signed short i,j;
+  /*   for(i=5;i>=1;i--){
+
+       for(j=1;j<=i;j++){
+         printf("*");
+       }
+       printf("\n");
+     }  */
+  /*   for(i=1; i<=5; i++){
+
+            for(j=5;j>=i;j--){
+              printf("*");
+            }
+            printf("\n");
+          }   */
+      /*    for(i=1; i<=5; i++){
+
+                 for(j=i;j<=i;j--){
+                   printf("*");
+                 }
+                 printf("\n");
+               } */
+          /*     for(i=1; i<=5; i++){
+
+                           for(j=i;j<=5;j++){
+                             printf("*");
+                           }
+                           printf("\n");
+                         }  */
+                    /*     for(i=5; i>=1; i--){
+
+                                     for(j=i;j>=1;j--){
+                                       printf("*");
+                                     }
+                                     printf("\n");
+                                   }  */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  }
